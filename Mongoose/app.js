94b145c5c4c1e21ae0ea29238ad312f4b7b56c22
Aman Kumar <kumaramansingh137@gmail.com>
2022-06-05 11:48:03 +0530

    Add files via upload

---
+++ Mongoose/app.js
@@ -0,0 +1,76 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
+
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit."
+});
+
+//fruit.save()
+
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+  name: "John",
+  age: 32
+});
+
+person.save();
+
+
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 10,
+  review: "The best fruit!"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 4,
+  review: "Too sour for me."
+});
+
+const banana = new Fruit({
+  name: "banana",
+  score: 3,
+  review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved all the fruits to fruitsDB");
+  }
+});
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some Documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits);
+    callback(fruits);
+  });
+}

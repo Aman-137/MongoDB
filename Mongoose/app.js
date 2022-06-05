07e50988c364a7ff45c5321783cf68d3bacae7cb
Aman Kumar <kumaramansingh137@gmail.com>
2022-06-05 17:03:50 +0530

    Add files via upload

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -54,23 +54,25 @@ const banana = new Fruit({
   review: "Weird texture"
 });
 
-Fruit.insertMany([kiwi, orange, banana], function(err){
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved all the fruits to fruitsDB");
+//   }
+// });
+
+
+Fruit.find(function(err, fruits){
   if (err) {
     console.log(err);
   } else {
-    console.log("Successfully saved all the fruits to fruitsDB");
+
+    mongoose.connection.close();
+    
+    fruits.forEach(function(fruit) {
+      console.log(fruit.name);
+    });
   }
+
 });
-
-
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some Documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  });
-}

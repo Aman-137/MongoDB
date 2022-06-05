f8578fcc0afb621f2bbec2966b42009e915d52ac
Aman Kumar <kumaramansingh137@gmail.com>
2022-06-05 18:02:17 +0530

    Add files via upload

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -4,8 +4,15 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
 const fruitSchema = new mongoose.Schema ({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please chech you data enty, no name is specified!"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
@@ -32,35 +39,44 @@ const person = new Person ({
   age: 32
 });
 
-person.save();
+//person.save();
 
 
 
 const kiwi = new Fruit({
   name: "Kiwi",
-  score: 10,
+  rating: 10,
   review: "The best fruit!"
 });
 
 const orange = new Fruit({
   name: "Orange",
-  score: 4,
+  rating: 4,
   review: "Too sour for me."
 });
 
 const banana = new Fruit({
   name: "banana",
-  score: 3,
+  rating: 3,
   review: "Weird texture"
 });
 
-// Fruit.insertMany([kiwi, orange, banana], function(err){
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfully saved all the fruits to fruitsDB");
-//   }
-// });
+
+
+// Create in mongodb
+
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved all the fruits to fruitsDB");
+  }
+});
+
+
+
+// Read in Mongodb
 
 
 Fruit.find(function(err, fruits){
@@ -68,11 +84,45 @@ Fruit.find(function(err, fruits){
     console.log(err);
   } else {
 
-    mongoose.connection.close();
-    
+    // mongoose.connection.close();
+
     fruits.forEach(function(fruit) {
       console.log(fruit.name);
     });
   }
 
+});
+
+
+// Update in Mongodb
+
+
+Fruit.updateOne({_id: "629c4811956bd4d367d1ddfb"}, {name: "Peach"}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully updated the document.");
+  }
+});
+
+
+
+// Delete in Mongodb
+
+
+Fruit.deleteOne({name: "Peach"}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully deleted the document.");
+  }
+});
+
+
+Person.deleteMany({name: "John"}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully deleted all the documents.");
+  }
 });

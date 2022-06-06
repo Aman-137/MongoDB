b16368aa1bb0d6a06e53de93a3978cbca9c59cc5
Aman Kumar <kumaramansingh137@gmail.com>
2022-06-06 11:34:12 +0530

    Add files via upload

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -29,17 +29,44 @@ const fruit = new Fruit ({
 
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favouriteFruit: fruitSchema
 });
 
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person ({
-  name: "John",
-  age: 32
+// Establishing relationship and Embedding Documents using Mongoose.
+
+
+// const pineapple = new Fruit({
+//   name: "Pineapple",
+//   score: 9,
+//   review: "Great fruit."
+// });
+
+const mango = new Fruit({
+  name: "Mango",
+  score: 10,
+  review: "The best fruit."
 });
 
-//person.save();
+mango.save();
+
+Person.updateOne({name: "John"}, {favouriteFruit: mango}, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully updated the document.");
+  }
+});
+
+// const person = new Person ({
+//   name: "Amy",
+//   age: 12,
+//   favouriteFruit: pineapple
+// });
+//
+// person.save();
 
 
 
@@ -65,14 +92,14 @@ const banana = new Fruit({
 
 // Create in mongodb
 
-
-Fruit.insertMany([kiwi, orange, banana], function(err){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully saved all the fruits to fruitsDB");
-  }
-});
+//
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved all the fruits to fruitsDB");
+//   }
+// });
 
 
 
@@ -97,32 +124,32 @@ Fruit.find(function(err, fruits){
 // Update in Mongodb
 
 
-Fruit.updateOne({_id: "629c4811956bd4d367d1ddfb"}, {name: "Peach"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully updated the document.");
-  }
-});
+// Fruit.updateOne({_id: "629c4811956bd4d367d1ddfb"}, {name: "Peach"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully updated the document.");
+//   }
+// });
 
 
 
 // Delete in Mongodb
 
 
-Fruit.deleteOne({name: "Peach"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully deleted the document.");
-  }
-});
-
-
-Person.deleteMany({name: "John"}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully deleted all the documents.");
-  }
-});
+// Fruit.deleteOne({name: "Peach"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted the document.");
+//   }
+// });
+//
+//
+// Person.deleteMany({name: "John"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted all the documents.");
+//   }
+// });
